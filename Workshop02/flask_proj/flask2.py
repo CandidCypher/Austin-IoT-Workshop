10c1fccdab7d16719b829b283120b23c430c30dc
Cameron Owens <cameronallanowens@gmail.com>
2016-02-21 13:29:54 -0600

    Writing Flask stuff

---
+++ Workshop02/flask_proj/flask2.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 cameron <cameron@default-VirtualBox>
+#
+# Distributed under terms of the MIT license.
+
+"""
+This is the first flask demo for the Austin IoT group
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
