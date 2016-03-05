8897e844b463ebce8669ea1d7691396bc72ec950
Cameron Owens <cameronallanowens@gmail.com>
2016-03-05 22:35:32 +0000

    Adding Intterupt version of Code

---
+++ Workshop03/RPI_GPIO/PIR_Interrupt.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 pi <pi@bumblebee>
+#
+# Distributed under terms of the MIT license.
+
+"""
+This Python Script is to show interrupts on the Pi.
+"""
+
+impo
+
+def Motion(PIR_PIN):
+    print("Motion T
