02e1391ad89e589793f9b891f5705492100cdb06
Cameron Owens <cameronallanowens@gmail.com>
2016-03-05 15:17:03 -0600

    Building up Workshop03

---
+++ Workshop03/README.md
@@ -0,0 +1,9 @@
+This directory contains all of the software and Python Scripts developed for the Austin IoT Workshops developed by Cameron Owens (www.candidcypher.com)
+
+The software developed is based upon the assumption that you are using the following software packages:
+
+Raspbian (Jessie-LITE)
+Anaconda/Miniconda Python
+
+
+If you need to set up your Raspberry Pi with all of the base software; please refer to the guides provided @ www.AustinIot.com
---
+++ Workshop03/RPI_GPIO/PIR_basic.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 cameron <cameron@cameron-Lenovo-ideapad-300-15ISK>
+#
+# Distributed under terms of the MIT license.
+
+"""
+This basic script displays basic messages if the PIR sensor dectects any
+motion. This was based off the demo stuff on Adafruit.
+"""
+
+import time
+import RPi.GPIO as io
+
+
+pir_pin = 26
+
+io.setup(pir_pin, io.IN)
+
+
+while True:
+    if io.input(pir_pin):
+        print("Motion Detected")
+    time.sleep(0.5)
