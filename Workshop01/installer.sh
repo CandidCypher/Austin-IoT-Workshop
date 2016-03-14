92010c57da44830531d4e28ef72810af664b663d
Cameron Owens <cameronallanowens@gmail.com>
2016-03-13 22:56:12 -0500

    Adding some more to the installer stuff. Pushing for a test.

--- Workshop01/installer.sh
+++ Workshop01/installer.sh
@@ -11,4 +11,13 @@ echo "This is a basic installer script for installing the software used in the
 Austin IoT Workshops. This script will automate the process of installing the 
 required software on your Raspberry Pi."
 
+sudo apt-get update && sudo apt-get upgrade
 
+cd ~/
+mkdir ~/Downloads
+cd ~/Downloads
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-armv7l.sh
+
+bash Mini*
+
+sudo apt-get install git mosquitto
