c6aedfaa42e99cf85c650256d9ae97c258f59fbd
Cameron Owens <cameronallanowens@gmail.com>
2016-04-13 22:42:27 -0500

    Adding the Arduino Blinky.

---
+++ Workshop03/MQTT_Stuff/MSGBlink/MessageRecieve.ino
@@ -0,0 +1,88 @@
+/*
+ * Simple MQTT Publisher for the NodeMCU
+ */
+#include <PubSubClient.h>
+# include <ESP8266WiFi.h>
+
+const char* ssid = "Cybertron";
+const char* password = "Bu22K!ll";
+
+const char* broker = "192.168.0.105";
+
+const int LED = 13;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+char msg[50];
+
+void setup_wifi()
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      //client.publish("Hello/", "hello world");
+      client.subscribe("BlinkTopic/");
+      // ... and resubscribe
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+ digitalWrite(LED, HIGH);
+ delay(1000);
+ digitalWrite(LED, LOW);
+ delay(1000); 
+}
+
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  setup_wifi();
+  client.setServer(broker, 1883);
+  client.setCallback(callback);
+}
+
+
+void loop() 
+{
+  if(!client.connected())
+  {
+    reconnect();
+  }
+
+  client.loop();
+  client.publish("Hello/", msg);
+}
