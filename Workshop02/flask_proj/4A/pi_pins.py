fc9fb5a01b140dab431af613a48d6748d81b270c
Cameron Owens <cameronallanowens@gmail.com>
2016-05-06 18:59:01 -0500

    Cleaning up some stuff for the flowerpot project on my personal site.

--- Workshop02/flask_proj/4A/pi_pins.py
+++ Workshop02/flask_proj/4A/pi_pins.py
@@ -73,4 +73,4 @@ def action(changePin, action):
    return render_template('main.html', **templateData)
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=True)
+   app.run(host='0.0.0.0', debug=True)
